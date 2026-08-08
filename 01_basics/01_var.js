b2d1974acb254ccc5bb22eb0e02c43ee9f21a5d2
Rushikesh Rane <rushikeshrane.dev@gmail.com>
2026-08-08 09:50:09 +0530

    Initial project setup

---
+++ 01_basics/01_var.js
@@ -0,0 +1,20 @@
+const accId = 1234;
+let accEmail = "abc@gmail.com";
+var accPass = "abc";
+accCity = "Jaipur";
+let accState;
+
+
+
+// accId = 4567; Not Possible //
+accEmail = "def@gmail.com";
+accPass = "cde";
+accCity = "Bengal";
+
+console.table([accId , accEmail , accPass , accCity , accState]);
+
+/* 
+Prefer not use var 
+because of issues in block and functionality scope
+*/
+
